e958c379a406dd3325e5d563e79fbecb17e416cb
FROBDUCK <100322056+FROBDUCK@users.noreply.github.com>
2024-08-30 20:04:38 -0300

    Add files via upload

---
+++ at3/Contato.java
@@ -0,0 +1,14 @@
+package at3;
+
+public class Contato {
+    String nome;
+    String telefone;
+    String email;
+
+    Contato(String nome, String telefone, String email){
+        this.nome=nome;
+        this.telefone=telefone;
+        this.email=email;
+    }
+    
+}
---
+++ at3/Main.java
@@ -0,0 +1,6 @@
+package at3;
+
+public class Main {
+    public static void main(String[] args){
+    }
+}
