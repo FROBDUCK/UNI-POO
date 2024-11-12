249fa12346ad10564bfd500d18fcf63b90f0e6f4
FROBDUCK <100322056+FROBDUCK@users.noreply.github.com>
2024-11-11 21:15:25 -0300

    Add files via upload

---
+++ poodia12/poo1211/src/Job.java
@@ -0,0 +1,47 @@
+public class Job {
+    private long id;
+    private String title;
+    private String description;
+    private float price;
+    private boolean availability;
+    private String dateOfCreation;
+
+    private Worker worker = new Worker();
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getDateOfCreation() {
+        return dateOfCreation;
+    }
+    public void setDateOfCreation(String dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+}
+
+
